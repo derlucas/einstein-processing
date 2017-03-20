68d53ef663e53f30c6a70beddfc735158165b58d
Lucas Pleß <hello@lucas-pless.com>
2017-03-20 17:13:24 +0100

    added note

--- strips/src/Strips.java
+++ strips/src/Strips.java
@@ -3,13 +3,18 @@ import oscP5.OscMessage;
 import oscP5.OscP5;
 import processing.core.PApplet;
 
+import java.awt.*;
+
 public class Strips extends PApplet {
 
-    private float ampFactor = 1.0f;
+    private float ampFactor = 10.0f;
     private int COUNT = 12;
     float amp[] = new float[12];
     OscP5 oscP5;
     private ControlP5 cp5;
+    private int note;
+
+
 
     public void settings() {
         size(800, 600);
@@ -17,7 +22,7 @@ public class Strips extends PApplet {
 
     public void setup() {
         oscP5 = new OscP5(this, 2001);
-        frameRate(60);
+        frameRate(30);
 
         cp5 = new ControlP5(this);
         cp5.addSlider("ampFactor").setPosition(10, 70).setSize(500, 20).setRange(0, 50.0f);
@@ -29,20 +34,21 @@ public class Strips extends PApplet {
 
         for (int i = 0; i < COUNT; i++) {
             fill(255);
-            text(amp[i] + "", 10, 120 + i * 20);
+            text(String.format("%.5f", amp[i]), 10, 120 + i * 20);
             stroke(50);
             fill(amp[i] * 255);
             rect(i * 65, 0, 60, 60);
         }
 
-//        float bla = 10*log(ampFactor+1) / log(10);
-//        fill(255);
-//        text("bla: " + bla, 100, 100);
+        fill(Color.HSBtoRGB(1.0f / note, 0.5f, 0.2f));
+        noStroke();
+        rect(200,200, 50,50);
     }
 
 
     void oscEvent(OscMessage msg) {
 
+//        System.out.println(msg.addrPattern());
         String addr = msg.addrPattern();
 
         if (addr.startsWith("amp")) {
@@ -51,15 +57,10 @@ public class Strips extends PApplet {
             amp[channel - 1] = amp[channel - 1] * ampFactor;
         }
 
-        if (msg.checkAddrPattern("keyNote")) {
-            int note = msg.get(0).intValue();
-            System.out.println("note: " + note);
+        if (msg.checkAddrPattern("/keyNote")) {
+            note = msg.get(0).intValue();
         }
 
-        if (msg.checkAddrPattern("keyVel")) {
-            int vel = msg.get(0).intValue();
-            System.out.println("vel =" + vel);
-        }
 
     }
 
